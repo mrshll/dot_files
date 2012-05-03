5db275d13bce14d1953a106c793f3543af48bf40
Marshall <mmoutenot@gmail.com>
2012-05-03 12:08:10 -0400

    fixing colors etc

---
+++ backup_dot_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp ~/.vimrc ~/.dot_files/.vimrc
+cp -rf ~/.vim ~/.dot_files/.vim
+cp ~/.bashrc ~/.dot_files/.bashrc
+cp ~/.bash_config ~/.dot_files/.bash_config
+cp ~/.bash_aliases ~/.dot_files/.bash_aliases
+cp ~/.bash_history_config ~/.dot_files/.bash_history_config
+cp ~/.gitconfig ~/.dot_files/.gitconfig
+cp ~/.screenrc ~/.dot_files/.screenrc
---
+++ update_dot_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cp  ~/.dot_files/.vimrc ~/.vimrc
+cp -r ~/.dot_files/.vim ~/.vim
+cp ~/.dot_files/.bashrc ~/.bashrc
+cp ~/.dot_files/.gitconfig ~/.gitconfig
+cp ~/.dot_files/.screenrc ~/.screenrc
+cp ~/.dot_files/.bash_aliases ~/.bash_aliases
+cp ~/.dot_files/.bash_history ~/.bash_history
+cp ~/.dot_files/.bash_config ~/.bash_config
